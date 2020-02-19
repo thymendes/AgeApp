20ae7a5c36db5e269db2b23a986cdeba2bce6df9
Thyago Mendes <thyagomendes@MacBook-Pro-de-Thyago.local>
2020-02-19 10:19:34 -0300

    Upload

---
+++ app/src/main/java/com/example/ageapp/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.ageapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun aoEnviar (view:View){
+        val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+        mostraIdade.text = "Age: " + (currentYear - inputYear.text.toString().toInt())
+    }
+}
+
